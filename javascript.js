44a67cfe0b289e27ec5ebaa3acab911656c80028
jarusoa <jarusoa@gmail.com>
2025-03-26 10:06:14 -0400

    Creating grid components in css

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ function createDivs(num){
     for(let i = 0; i < num; i++){
         // Create divs for one row of the num x num grid, must have a container to display in a row using flex box.
         var rowContainer = document.createElement("div");
+        rowContainer.classList.add("rowContainer");
         for(let i = 0; i < num; i ++){
             var div = document.createElement("div");
             div.classList.add("square");
